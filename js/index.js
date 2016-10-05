e89948c0fd8d514e674279f2a709f4efdd5db7c6
緒方誠 <ogatamakoto1976@gmail.com>
2016-10-06 00:57:09 +0900

    fix javascript file

--- js/index.js
+++ js/index.js
@@ -1,18 +1,31 @@
-
 var i = 1;
+var pageIndex = $('.main-sentence').length;
 $('#page' + i).show();
+$('#previous').prop('disabled', true);
+
 
 $('#next').on('click', function(){
 		i++;
-		var nextPage = $('#page' + i).show();
-		$(nextPage).prev().hide();
+		$('#previous').prop('disabled', false);
+		if( i <= pageIndex ){
+			var nextPage = $('#page' + i).show();
+			$(nextPage).prev().hide();
+		}
+		if( i == pageIndex ){
+			$('#next').prop('disabled', true);
+		}
+		//	console.log(i);
 });
+
 
 $('#previous').on('click', function(){
 		i--;
 		var prevPage = $('#page' + i).show();
 		$(prevPage).next().hide();
+		$('#next').prop('disabled', false);
+
+		if(i == 1){
+			$('#previous').prop('disabled', true);
+		}
 });
-
-
 
