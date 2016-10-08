17e0650ef06eed89aa1349eb9547cf2f9d10ff8f
緒方誠 <ogatamakoto1976@gmail.com>
2016-10-08 16:52:54 +0900

    fix javascript file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,51 @@
+function sentence (){
+
+	var sentenceContent = {
+		url: 'sentence.json',
+		dataType: 'json',
+		success: function(result) {
+				$.each( result, function(index, elem){
+						$('<div class="main-sentence"><h3>'+ elem.title +'</h3><p>' + elem.content + '</p></div>').appendTo('.content-sentence');
+				});
+				var i = 1;
+				var pageIndex = $('.main-sentence').length;
+				$('#previous').prop('disabled', true);
+				$('.main-sentence:nth-child(1)').show();
+
+				$('#next').on('click', function(){
+						i++;
+						$('#previous').prop('disabled', false);
+						if( i <= pageIndex ){
+							var nextPage = $('.main-sentence:nth-child(' + i + ')').show();
+							$(nextPage).prev().hide();
+						}
+						if( i == pageIndex ){
+							$('#next').prop('disabled', true);
+						}
+						//	console.log(i);
+				});
 
 
+				$('#previous').on('click', function(){
+						i--;
+						var prevPage = $('.main-sentence:nth-child(' + i + ')').show();
+						$(prevPage).next().hide();
+						$('#next').prop('disabled', false);
+
+						if(i == 1){
+							$('#previous').prop('disabled', true);
+						}
+				});
+		},
+		error: function(){
+			alert('読み込めませんでした');
+		}
+	}
+
+	$.ajax(sentenceContent);
+}
+
+sentence();
 
 /*
 var i = 1;
