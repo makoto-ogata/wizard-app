4e4cc0ff8b206d9e5a3ca781cebaa48247d76cf0
緒方誠 <ogatamakoto1976@gmail.com>
2016-10-09 14:54:34 +0900

    make button function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+function pageAction (){
+	var i = 1;
+	var pageIndex = $('.main-sentence').length;
+	$('#previous').prop('disabled', true);
+	$('.main-sentence:nth-child(1)').show();
+
+	$('#next').on('click', function(){
+			i++;
+			$('#previous').prop('disabled', false);
+			if( i <= pageIndex ){
+				var nextPage = $('.main-sentence:nth-child(' + i + ')').show();
+				$(nextPage).prev().hide();
+			}
+			if( i == pageIndex ){
+				$('#next').prop('disabled', true);
+			}
+			//	console.log(i);
+	});
+
+
+	$('#previous').on('click', function(){
+			i--;
+			var prevPage = $('.main-sentence:nth-child(' + i + ')').show();
+			$(prevPage).next().hide();
+			$('#next').prop('disabled', false);
+
+			if(i == 1){
+				$('#previous').prop('disabled', true);
+			}
+	});
+}
+
+
+
 function sentence (){
 
 	var sentenceContent = {
@@ -7,35 +41,7 @@ function sentence (){
 				$.each( result, function(index, elem){
 						$('<div class="main-sentence"><h3>'+ elem.title +'</h3><p>' + elem.content + '</p></div>').appendTo('.content-sentence');
 				});
-				var i = 1;
-				var pageIndex = $('.main-sentence').length;
-				$('#previous').prop('disabled', true);
-				$('.main-sentence:nth-child(1)').show();
-
-				$('#next').on('click', function(){
-						i++;
-						$('#previous').prop('disabled', false);
-						if( i <= pageIndex ){
-							var nextPage = $('.main-sentence:nth-child(' + i + ')').show();
-							$(nextPage).prev().hide();
-						}
-						if( i == pageIndex ){
-							$('#next').prop('disabled', true);
-						}
-						//	console.log(i);
-				});
-
-
-				$('#previous').on('click', function(){
-						i--;
-						var prevPage = $('.main-sentence:nth-child(' + i + ')').show();
-						$(prevPage).next().hide();
-						$('#next').prop('disabled', false);
-
-						if(i == 1){
-							$('#previous').prop('disabled', true);
-						}
-				});
+				pageAction();
 		},
 		error: function(){
 			alert('読み込めませんでした');
